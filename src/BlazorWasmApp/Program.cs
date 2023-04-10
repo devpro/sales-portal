58168fdc085a1dddbff8404da7db7aa6495a104a
Bertrand Thomas <bertrand@devpro.fr>
2023-04-10 21:51:20 +0200

    Initialize from .NET template

---
+++ src/BlazorWasmApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Devpro.SalesPortal.BlazorWasmApp;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// TODO: get this information from a user setting
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
