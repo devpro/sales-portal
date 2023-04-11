ffe7411e8132a40b273c6cc0493a20dc0a292290
Bertrand Thomas <bertrand@devpro.fr>
2023-04-11 16:47:27 +0200

    Add CRM Adapter Web API from ASP.NET template

---
+++ src/CrmAdapterWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using Devpro.SalesPortal.SalesDomain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Devpro.SalesPortal.CrmAdapterWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecastDto> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecastDto
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ src/SalesDomain/Dtos/WeatherForecastDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Devpro.SalesPortal.SalesDomain
+{
+    public class WeatherForecastDto
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
