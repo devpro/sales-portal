fcf69b908c8ec2f8269ea490dec3d09fcebf9913
Bertrand Thomas <bertrand@devpro.fr>
2024-02-16 14:42:45 +0100

    Small code improvement

--- src/CrmDataWebApi/Repositories/CrudRepositoryBase.cs
+++ src/CrmDataWebApi/Repositories/CrudRepositoryBase.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Devpro.SalesPortal.CrmDataWebApi.Repositories
@@ -27,13 +26,13 @@ namespace Devpro.SalesPortal.CrmDataWebApi.Repositories
         {
             var collection = GetCollection<U>();
             var dbEntries = await collection.FindAsync(new BsonDocument());
-            return Mapper.Map<List<T>>(dbEntries.ToList());
+            return Mapper.Map<List<T>>(dbEntries.ToList()) ?? [];
         }
 
         public async Task<T> CreateAsync(T input)
         {
             var collection = GetCollection<U>();
-            var entity = Mapper.Map<U>(input);
+            var entity = Mapper.Map<U>(input) ?? throw new ArgumentNullException(nameof(input));
             await collection.InsertOneAsync(entity);
             return Mapper.Map<T>(entity);
         }
@@ -41,23 +40,21 @@ namespace Devpro.SalesPortal.CrmDataWebApi.Repositories
         public async Task UpdateAsync(string id, T input)
         {
             var objectId = ParseObjectId(id);
-            var collection = GetCollection<U>();
             var entity = Mapper.Map<U>(input);
-            await collection.ReplaceOneAsync(x => x.Id == objectId, entity);
+            await GetCollection<U>().ReplaceOneAsync(x => x.Id == objectId, entity);
         }
 
         public async Task DeleteAsync(string id)
         {
             var objectId = ParseObjectId(id);
-            var collection = GetCollection<U>();
-            await collection.DeleteOneAsync(x => x.Id == objectId);
+            await GetCollection<U>().DeleteOneAsync(x => x.Id == objectId);
         }
 
-        protected static ObjectId ParseObjectId(string id, string message = "")
+        protected static ObjectId ParseObjectId(string id)
         {
             if (string.IsNullOrEmpty(id) || !ObjectId.TryParse(id, out var objectId))
             {
-                throw new ArgumentException($"{message}{id} is not a valid id.", nameof(id));
+                throw new ArgumentException($"{id} is not a valid object id.", nameof(id));
             }
 
             return objectId;
